82f4cc82542e20e8198af041c57ad0af6257efe5
lilizav <vuthiliza@gmail.com>
2025-05-10 14:43:41 +0200

    lesson 26

---
+++ lesson 26/js/main.js
@@ -0,0 +1,118 @@
+console.log("hello there")
+var x=5;
+console.log(x)
+var y=100;
+console.log(x+y)
+var a=20;
+var b=30;
+var c=a+b;
+console.log(c)
+var a=154;
+console.log(a)
+
+var l=12;
+var o=6;
+
+console.log(l-o)
+console.log(l/o)
+console.log(l*o)
+
+n=10;
+s=3;
+result=n%s;
+
+console.log(result)
+console.log(12%3)
+
+numri=100;
+numri--;
+numri--;
+
+console.log(numri)
+
+numri--;
+console.log(numri)
+
+number=100;
+number++;
+console.log(number)
+
+x--;
+console.log(x)
+
+console.log("------------")
+x=15;
+x+=5;
+
+console.log(x)
+
+console.log("------------")
+y=20;
+y-=5;
+
+console.log(y)
+
+console.log("------------")
+var k=20;
+k*=2;
+
+console.log(k)
+
+console.log("------------")
+l=20;
+l/=10;
+
+console.log(l)
+
+console.log("------------Pjesa e dyte------------")
+
+x=5;
+y=5;
+
+console.log(x==y)
+
+e=9;
+f=5;
+
+console.log(e==f)
+
+p=4;
+u=7;
+
+console.log(p!=u)
+
+m=12;
+t=12;
+
+console.log(m!=t)
+
+console.log("------------")
+
+num1=120;
+num2=300;
+
+console.log(num1>num2)
+
+console.log("------------")
+
+console.log(num1<num2)
+
+console.log("------------")
+
+var age=27;
+
+console.log(age>15 && age<80)
+
+console.log("------------")
+
+var var1=7;
+var var2=12;
+
+console.log(var1==12 || var2==12)
+
+console.log("------------")
+
+var firstName= "Liza";
+var lastName="Vuthi";
+
+console.log(firstName+" "+lastName)
