0f41d5f7979aba33c93474940000779470f6c1fa
lilizav <vuthiliza@gmail.com>
2025-06-07 14:41:24 +0200

    lesson 30

--- lesson 30/part 1/js/main.js
+++ lesson 30/part 1/js/main.js
@@ -1,18 +1,8 @@
-// var x = -2;
-
-// if(x>2){
-//     console.log("x eshte vlere me e madhe se 2")
-// }else if(x>-3){
-//     console.log("x ehste me i madh se -3")
-// }
-// else{
-//     console.log("asnje kusht nuk eshte plotesuar")
-// }
 
 
-  var input = document.getElementById("input_id");
-   var button = document.getElementById("btn_id");
-   var text = document.getElementById("text_id");
+var input = document.getElementById("input_id");
+var button = document.getElementById("btn_id");
+var text = document.getElementById("text_id");
 
    button.onclick=function(){
     text.innerHTML=input.value;
@@ -28,3 +18,12 @@
    button2.addEventListener("click",function(){
     ans.innerHTML = parseInt(input1.value)+parseInt(input2.value);
    });
+
+   button.onclick =function(){
+    if(input.value>10){
+      text.innerHTML="input value is greater than 10"
+    }
+    else if(input.value<10){
+      text.innerHTML="input value is less than 10"
+    }
+   }
